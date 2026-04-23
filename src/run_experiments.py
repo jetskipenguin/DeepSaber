8dd0a34197d6a308f4c9db59ce0a893d8723618d
Collin <collinnewman12@gmail.com>
2026-04-23 17:15:33 -0500

    Revert "Only run experiments for main models"
    This reverts commit f49ddc74d09514a6df41057fb87e764fa69c4b65.

--- src/run_experiments.py
+++ src/run_experiments.py
@@ -6,19 +6,10 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import experiments
 
 if __name__ == '__main__':
-    print("--- Starting Baseline Model Evaluation ---")
+    experiments.custom_model.main()
     experiments.baseline_model.main()
-    
-    print("--- Starting DDC Model Evaluation ---")
     experiments.ddc_model.main()
-    
-    print("--- Starting Hypersearch (CLSTM/MLSTM) Evaluation ---")
+    experiments.best_model_comparison.main()
     experiments.hypersearch_model.main()
-
-    # --- Skipped Experiments ---
-    # experiments.custom_model.main()
-    #experiments.best_model_comparison.main()
-    # experiments.information_comparison.main()
-    # experiments.temperature_search.main()
-    
-    print("--- All specified experiments completed. ---")
+    experiments.information_comparison.main()
+    experiments.temperature_search.main()
