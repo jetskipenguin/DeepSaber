f49ddc74d09514a6df41057fb87e764fa69c4b65
Collin <collinnewman12@gmail.com>
2026-04-23 16:03:10 -0500

    Only run experiments for main models

--- src/run_experiments.py
+++ src/run_experiments.py
@@ -6,10 +6,19 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import experiments
 
 if __name__ == '__main__':
-    experiments.custom_model.main()
+    print("--- Starting Baseline Model Evaluation ---")
     experiments.baseline_model.main()
+    
+    print("--- Starting DDC Model Evaluation ---")
     experiments.ddc_model.main()
-    experiments.best_model_comparison.main()
+    
+    print("--- Starting Hypersearch (CLSTM/MLSTM) Evaluation ---")
     experiments.hypersearch_model.main()
-    experiments.information_comparison.main()
-    experiments.temperature_search.main()
+
+    # --- Skipped Experiments ---
+    # experiments.custom_model.main()
+    #experiments.best_model_comparison.main()
+    # experiments.information_comparison.main()
+    # experiments.temperature_search.main()
+    
+    print("--- All specified experiments completed. ---")
